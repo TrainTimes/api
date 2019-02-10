5e13e73436e1d16d3f5cbf4ed60091eda4c08d15
Clive Cornelous Banditi <clive@cornelo.us>
2019-02-10 21:20:23 +0200

    -- update --

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -2,51 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Log;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ShopifyController extends Controller
 {
     public function install(Request $request)
     {
-//      $shopifyClient = new ShopifyClient();
-//        Log::debug('>> Shopify App Installation >>');
+        $pre = __METHOD__ . ' : ';
+        Log::debug($pre . ' >> Shopify App Installation >> . bof');
+
         $data = $request->all();
+        Log::debug($pre . ' ' . print_r($data, true));
 
         $api_key = env('SHOPIFY_API_KEY');
-        $api_secret = env('SHOPIFY_API_SECRET');
         $scopes = "read_orders,read_products,write_products";
-        $redirect_uri = "https://www.traintimes.co.za/shopify/generate_token.php";
+        $redirect_uri = "https://www.traintimes.co.za/shopify/generatetoken";
         $shop = $data['shop'];
         $install_url = "https://www." . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" .
             $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 
+        Log::debug($pre . ' >> Shopify App Installation >> . eof');
         header("Location: " . $install_url);
         die();
     }
 
     public function generateToken(Request $request)
     {
-
+        $pre = __METHOD__ . ' : ';
         $data = $request->all();
-
-        return $data;
-//        // Set variables for our request
-//        $shared_secret = "TBB5wltKarRtKn5mUVZck9RxHePNN6Jo";
-//        $params = $_GET; // Retrieve all request parameters
-//        $hmac = $_GET['hmac']; // Retrieve HMAC request parameter
-//        $params = array_diff_key($params, array('hmac' => '')); // Remove hmac from params
-//        ksort($params); // Sort params lexographically
-//
-//        // Compute SHA256 digest
-//        $computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
-//
-//        // Use hmac data to check that the response is from Shopify or not
-//        if (hash_equals($hmac, $computed_hmac)) {
-//            // VALIDATED
-//        } else {
-//            // NOT VALIDATED – Someone is trying to be shady!
-//        }
+        Log::debug($pre . ' >> Shopify App Generate Token >> . bof');
+        Log::debug($pre . ' ' . print_r($data, true));
+        Log::debug($pre . ' >> Shopify App Generate Token >> . eof');
     }
 
 }
