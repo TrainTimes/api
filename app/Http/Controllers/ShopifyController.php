62ec38595a6fc3eceaeab9ffb1719587d6a931e8
Clive Cornelous Banditi <clive@cornelo.us>
2019-02-10 22:03:19 +0200

    -- update --

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -33,7 +33,26 @@ class ShopifyController extends Controller
         $data = $request->all();
         Log::debug($pre . ' >> Shopify App Generate Token >> . bof');
         Log::debug($pre . ' ' . print_r($data, true));
+
+        // validate data
+        // Set variables for our request
+        $shared_secret = env('SHOPIFY_API_SECRET');
+        $params = $_GET; // Retrieve all request parameters
+        $hmac = $data['hmac']; // Retrieve HMAC request parameter
+        $params = array_diff_key($params, array('hmac' => '')); // Remove hmac from params
+        ksort($data); // Sort params lexographically
+
+        // Compute SHA256 digest
+        $computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
+
+        // Use hmac data to check that the response is from Shopify or not
+        if (hash_equals($hmac, $computed_hmac)) {
+            Log::debug($pre . ' >> DATA is valid, you can get the access token >>');
+            // get access_token
+        } else {
+            Log::debug($pre . ' >> DATA is NOT valid, Someone is trying to be shady! >>');
+        }
+
         Log::debug($pre . ' >> Shopify App Generate Token >> . eof');
     }
-
 }
